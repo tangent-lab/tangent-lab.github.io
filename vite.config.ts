e7e15e797ce7ae189f893129543f550abac940b1
Timothy Jaeryang Baek <tim@openwebui.com>
2024-11-24 20:51:02 -0800

    refac

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "path";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "../build/",
+        entryFileNames: "main.js",
+        assetFileNames: "style.css",
+        chunkFileNames: "chunk.js",
+        manualChunks: undefined,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
 })
