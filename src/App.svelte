7a9a3507cdc5a6d34f92bdaa61f119e9d057b4df
Timothy Jaeryang Baek <tim@openwebui.com>
2024-11-24 21:30:39 -0800

    feat: contact

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,25 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script lang="ts">
+  import Contact from "$lib/components/Contact.svelte";
+</script>
+
+<div class="h-screen max-h-[100dvh] w-screen flex justify-center">
+  <div class="w-full max-w-6xl p-2.5">
+    <div class="flex items-center gap-2 mt-4 mb-4">
+      <div class="text-2xl font-semibold text-gray-700">Tangent Lab</div>
+
+      <div>
+        <img src="/favicon.png" alt="Tangent Lab" class="size-8" />
+      </div>
+    </div>
+
+    <div class="">
+      <img
+        src="/assets/images/sfu.webp"
+        alt="Tangent Lab"
+        class="rounded-lg h-96 w-full bg-inherit object-cover"
+      />
+    </div>
+
+    <Contact />
+  </div>
+</div>
---
+++ src/lib/components/Contact.svelte
@@ -0,0 +1,34 @@
+<div class="my-4">
+  <div class="font-semibold mb-1">Contact</div>
+
+  <div class="flex gap-2.5">
+    <div class="basis-1/2">
+      <div class="text-sm text-gray-600 mb-2">
+        Tangent Lab is part of the SFU IxLab group and focuses on research in
+        Human-Computer Interaction (HCI). Our lab is located in the Technology
+        and Science Complex 1 (TASC1) on the Burnaby campus.
+      </div>
+
+      <div>
+        <img
+          src="/assets/images/tasc1.png"
+          alt="Tangent Lab"
+          class="rounded-lg h-96 w-full bg-inherit object-cover"
+        />
+      </div>
+    </div>
+
+    <div class="basis-1/2">
+      <iframe
+        class="rounded-lg"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.206447259982!2d-122.91669278709588!3d49.27672497105547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548679bf39213df3%3A0x851019771abbd8cd!2sTechnology%20and%20Science%20Complex%201%2C%20Burnaby%2C%20BC%20V5A%201S6!5e1!3m2!1sen!2sca!4v1732512301786!5m2!1sen!2sca"
+        width="100%"
+        height="100%"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  </div>
+</div>
