f600bfddb4e84c4ff34208bcd3f44f9622ce8230
xueying720 <xza324@sfu.ca>
2025-01-27 05:05:11 +0000

    Update website structure

--- src/lib/components/Publications.svelte
+++ src/lib/components/Publications.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
   let { publications, showList = true } = $props();
 
   const limitedPublications = publications.slice(0, 6);
 
   let selectedYear = 'All';
   let selectedType = 'All';
+
 
   function getFilteredPublications() {
     return publications.filter(pub => {
@@ -21,7 +26,7 @@
     let groupMap = {};
 
     filtered.forEach(pub => {
-      const y = pub.year.toString(); // 统一转字符串，避免 key 混乱
+      const y = pub.year.toString(); 
       if (!groupMap[y]) {
         groupMap[y] = [];
       }
@@ -35,10 +40,8 @@
     const groups = getGroupedPublications();
     const keys = Object.keys(groups);
     keys.sort((a, b) => {
-      // 把 'Preprint' 放在最前面，或者你想要的其他策略
       if (a === 'Preprint') return -1;
       if (b === 'Preprint') return 1;
-      // 否则按数值从大到小排
       return parseInt(b) - parseInt(a);
     });
 
@@ -72,10 +75,136 @@
 
 <div class="my-4">
 
-  {#if showList}
-    <div class="relative flex">
-      <div class="flex gap-4 mb-6 flex-wrap">
-        <!-- 
+  {#if showList} 
+  <div class="relative flex">
+    <div class="flex gap-4 mb-6 flex-wrap">
+      <!-- 
+      {#each getSortedYearKeys() as year}
+        <div id={"year-" + year} class="mb-2">
+          <div class="my-4 text-xl font-bold">{year}</div>
+          <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            {#each getGroupedPublications()[year] as pub}
+              <div
+                class="relative flex flex-col justify-center rounded-lg group cursor-pointer"
+              >
+                <img
+                  src={pub?.image ?? "/assets/images/user.png"}
+                  alt={pub.title}
+                  class="rounded-lg w-full aspect-square object-cover"
+                />
+
+                <div
+                  class="absolute rounded-lg top-0 left-0 w-full h-full bg-gradient-to-t from-gray-800 to-transparent text-white text-xs font-medium bottom-0 p-1"
+                ></div>
+
+                <div class=" absolute z-20 bottom-0 text-white p-5">
+                  <div class="text-sm text-gray-100 font-medium">
+                    {pub.venue}
+                  </div>
+                  <div
+                    class=" mt-1 mb-1.5 group-hover:underline font-medium cursor-pointer"
+                  >
+                    <a href={`/publications/${pub.id}`} target="_blank" rel="noopener noreferrer">
+                      {pub.title}
+                    </a>
+                  </div>
+                  <div class="text-sm text-gray-400 line-clamp-2">
+                    {pub.authors}
+                  </div>
+
+                  <div class="flex gap-1.5 mt-1">
+                    {#each pub.links as link}
+                      <a
+                        href={link.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        class="text-sm text-gray-100 hover:underline"
+                      >
+                        {link.title}
+                      </a>
+                    {/each}
+                  </div>
+                </div>
+              </div>
+            {/each}
+          </div>
+        </div>
+      {/each}
+      -->
+      {#each getSortedYearKeys() as year}
+        <div id={"year-" + year} class="mb-2">
+          <div class="font-secondary font-bold text-2xl mb-6 ">{year}</div>
+
+          {#each getGroupedPublications()[year] as pub}
+            <div class="gap-4 mb-4 flex w-full justify-between items-top">
+              <img
+                  src={pub?.image ?? "/assets/images/news.png"}
+                  alt={pub.title}
+                  class="rounded-lg w-1/4 aspect-w-4 aspect-h-3 object-cover"
+                />
+              <div class="w-3/4 px-8 py-6 bg-gray-50 rounded-lg cursor-pointer hover:shadow-md transition-shadow ">
+                <div class="text-sm font-medium mb-2">{pub.venue}</div>
+                <a
+                    href="#"
+                    onclick={() => {
+                      event.preventDefault();
+                      dispatch('selectPublication', pub);
+                    }}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="font-secondary font-medium"
+                  >
+                  {pub.title}
+                </a>
+                <div class="text-sm mt-2 mb-4">{pub.authors}</div>
+                <div class="flex gap-2 mt-4">
+                  <a
+                      href="#"
+                      onclick={() => {
+                        event.preventDefault();
+                        dispatch('selectPublication', pub);
+                      }}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      class="
+                        text-xs font-medium p-2 text-xs text-gray-900 bg-white bg-opacity-60
+                        items-center          
+                        justify-center 
+                        hover:bg-[#A6192E]    
+                        hover:text-white    
+                        rounded-md         
+                        transition-colors
+                        duration-200"
+                    >
+                      Details
+                  </a>
+                  {#each pub.links as link}
+                    <a
+                      href={link.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      class="
+                        text-xs font-medium p-2 text-xs text-gray-900 bg-white bg-opacity-60
+                        items-center          
+                        justify-center 
+                        hover:bg-[#A6192E]    
+                        hover:text-white     
+                        rounded-md         
+                        transition-colors
+                        duration-200"
+                    >
+                      {link.title}
+                    </a>
+                  {/each}
+                </div>
+              </div>
+            </div>
+          {/each}
+          </div>
+      {/each}
+    </div>
+  </div>
+  <!-- 
         <div class="flex w-full max-w-6xl gap-4">
           <div class="flex-1 max-w-md gap-x-4	mt-1 mb-1.5 font-medium">
             Year
@@ -149,26 +278,8 @@
             </div>
           </div>
         </div>
-        -->
-    
-        
-        
-        {#each getSortedYearKeys() as year}
-        <div id={"year-" + year} class="mb-2">
-          <div class="font-bold">{year}</div>
-
-          {#each getGroupedPublications()[year] as pub}
-            <article style="margin: 0.5rem 0;">
-              <h3>{pub.title}</h3>
-              <p><em>{pub.type}</em> | {pub.venue}</p>
-              <p>{pub.authors}</p>
-            </article>
-          {/each}
-          </div>
-      {/each}
+          
       </div>
-  
-  <!-- 
   <aside class="hidden md:block ml-auto text-right">
     <nav class="fixed mt-20 flex flex-col gap-2">
       {#each getSortedYearKeys() as y}
@@ -182,12 +293,6 @@
     </nav>
   </aside>
   -->
-</div>
-
-    
-
-    
-
 
 
   {:else}
@@ -196,7 +301,7 @@
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
       {#each limitedPublications as publication}
         <div
-          class="relative flex flex-col justify-center rounded-lg group cursor-pointer"
+          class="relative flex flex-col justify-center rounded-lg group cursor-pointer" 
         >
           <img
             src={publication?.image ?? "/assets/images/user.png"}
@@ -215,15 +320,35 @@
             <div
               class=" mt-1 mb-1.5 group-hover:underline font-medium cursor-pointer"
             >
-              <a href={publication.url} target="_blank" rel="noopener noreferrer">
+              <a
+                  href=""
+                  onclick={() => {
+                    event.preventDefault();
+                    dispatch('selectPublication', publication);
+                  }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                 {publication.title}
-              </a>
+                </a>
             </div>
             <div class="text-sm text-gray-400 line-clamp-2">
               {publication.authors}
             </div>
 
             <div class="flex gap-1.5 mt-1">
+              <a
+                  href="#"
+                  onclick={() => {
+                    event.preventDefault();
+                    dispatch('selectPublication', publication);
+                  }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="text-sm text-gray-100 hover:underline"
+                >
+                  Details
+              </a>
               {#each publication.links as link}
                 <a
                   href={link.url}
